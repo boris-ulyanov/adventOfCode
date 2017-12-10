2b02f9b1462de8972d99f019c577403b2060dba6
Boris Ulyanov <boris-ulyanov@mail.ru>
2017-12-10 11:02:32 +0300

    day 9

---
+++ day-09/2.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+data_file = open('./data', 'r')
+data_lines = data_file.readlines()
+data_file.close()
+
+buff = ''.join(data_lines)
+buff_len = len(buff)
+
+data_lines = None
+
+print 'buff len', len(buff)
+
+level = 0
+level_sum = 0
+state = 'body'
+idx = 0
+garbage_sum = 0
+
+while True:
+
+    if idx >= buff_len:
+        print 'EOF'
+        break
+
+    sym = buff[idx]
+    idx += 1
+
+    if state == 'body':
+
+        if sym == '{':
+            level += 1
+            level_sum += level
+
+        elif sym == '}':
+            level -= 1
+
+        elif sym == '!':
+            idx += 1
+
+        elif sym == '<':
+            state = 'garbage'
+
+    elif state == 'garbage':
+
+        if sym == '!':
+            idx += 1
+
+        elif sym == '>':
+            state = 'body'
+
+        else:
+            garbage_sum += 1
+
+
+print 'level', level
+print 'level_sum', level_sum
+print 'state', state
+print 'garbage_sum', garbage_sum
